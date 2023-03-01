0bc463a22e67b20f69d63722a1ed6b6d1022a0f1
AEmin321 <ahmademin321@gmail.com>
2023-03-01 23:55:56 +0300

    Added the design for header,footer and fixed add button

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+let myLibrary = [
+    {
+        title:"filani",
+        author:"filanias",
+        pages:324,
+        read:false
+    },
+    {
+        title:"alksdjf",
+        author:"alksdjkf;la",
+        pages:22,
+        read:true
+    }
+];
+
+function Book () {
+    // constructor goes here
+}
+
+function addBookToLibrary () {
+
+}
+
+function displayBooks () {
+
+}
