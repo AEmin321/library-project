931fb138b6841319ccfd62b3f1877b8c6b5cf342
AEmin321 <ahmademin321@gmail.com>
2023-03-02 01:18:12 +0300

    Added the display function to show array of objects items to the table

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+const mainSection = document.querySelector('main');
 
 let myLibrary = [
     {
@@ -12,8 +12,16 @@ let myLibrary = [
         author:"alksdjkf;la",
         pages:22,
         read:true
+    },
+    {
+        title:"fukin",
+        author:"no way im",
+        pages:222,
+        read:true
     }
 ];
+
+displayBooks();
 
 function Book () {
     // constructor goes here
@@ -24,5 +32,42 @@ function addBookToLibrary () {
 }
 
 function displayBooks () {
+    myLibrary.forEach((item)=>{
+        const bookCard=document.createElement('div');
+        const bookName=document.createElement('p');
+        const bookAuthor=document.createElement('p');
+        const bookPages=document.createElement('p');
+        const bookStatus=document.createElement('button');
+        const bookRemove=document.createElement('button');
 
+        bookCard.classList.add('book-card');
+        bookName.classList.add('book-name');
+        bookAuthor.classList.add('book-author');
+        bookPages.classList.add('book-pages');
+        bookStatus.classList.add('book-status');
+        bookRemove.classList.add('book-remove');
+
+        bookName.textContent=item.title;
+        bookAuthor.textContent=item.author;
+        bookPages.textContent=item.pages;
+        bookStatus.textContent=handleBookStatus(item.read);
+        bookRemove.textContent="Remove";
+
+        bookCard.appendChild(bookName);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPages);
+        bookCard.appendChild(bookStatus);
+        bookCard.appendChild(bookRemove);
+
+        mainSection.appendChild(bookCard);
+    })
+}
+
+function handleBookStatus (item) {
+    if (item){
+        return "Read";
+    }
+    else {
+        return "Not Read";
+    }
 }
