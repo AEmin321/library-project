89910547078da6f5b1d6104a4cc840f021b859f0
AEmin321 <ahmademin321@gmail.com>
2023-03-02 22:19:43 +0300

    Added the function to get data from form and add to object array

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const mainSection = document.querySelector('main');
+const form = document.querySelector('#form');
+
 
 let myLibrary = [];
 
-addBookToLibrary();
-displayBooks();
+
 
 
 
@@ -14,15 +15,33 @@ function Book (title,author,pages,read) {
     this.read=read;
 }
 
+// adding the input values to the library object array
 function addBookToLibrary () {
-    let book1=new Book("Hello","iamtheauthor",123,true);
-    let book2=new Book("bye","meamo",322,true);
-    myLibrary.push(book1,book2);
+    form.addEventListener('submit',(event)=>{
+        event.preventDefault();
+        const elements=event.target.elements;
+        const bookName=elements.title.value;
+        const bookAuthor=elements.author.value;
+        const bookPages=elements.pages.value;
+        const bookStatus=elements.status.checked;
+
+        const newBook=new Book(bookName,bookAuthor,bookPages,bookStatus);
+        myLibrary.push(newBook);
+
+        displayBooks();
+    })
 }
+
+
+addBookToLibrary();
+
 
 
 // function to display the books list to table
 function displayBooks () {
+    //reseting the html card divs
+    resetDisplay();
+
     myLibrary.forEach((item)=>{
         const bookCard=document.createElement('div');
         const bookName=document.createElement('p');
@@ -54,6 +73,14 @@ function displayBooks () {
     })
 }
 
+// reseting display
+function resetDisplay () {
+    const cards=document.querySelectorAll('.book-card');
+    cards.forEach((card)=>{
+        card.remove();
+    })
+}
+
 
 // handles the book status read or not read
 function handleBookStatus (item) {
@@ -64,3 +91,6 @@ function handleBookStatus (item) {
         return "Not Read";
     }
 }
+
+
+
